9a83ce8748bcfa216faf94e9a5953d23e5e36c12
Pi <cpranaviyer@gmail.com>
2021-02-20 01:42:38 -0700

    edits

--- HW2/EuclideanALg.java
+++ HW2/EuclideanALg.java
@@ -1,7 +1,24 @@
 package HW2;
 
+import java.util.Arrays;
+import java.util.*;
+import java.io.*;
+
+//GCD with Euclidean Algorithm
+
 public class EuclideanALg {
     public static void main(String[] args) {
-        
-    } 
+        int pFive[]={2105,425};
+        toMod(pFive);
+
+    }
+    public static int toMod(int tMod[]) {
+        int gcd=0;
+        //check is array[0] is the largest and if not change order
+        if (tMod[0]<tMod[1]){
+            
+        }
+
+        return gcd;
+    }
 }
